92c8a5ed00cf7b07abd369af339696c5a7e06c33
JessP070 <jcpereira070@gmail.com>
2020-08-01 19:41:06 -0700

    create users_controller function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    #render login form
+    get "/login" do
+        erb :login
+
+    end
+    
+    post "/login" do
+        #find the user
+        user = User.find_by(email: params[:email])
+        #authenticate the user
+        if user && user.authenticate(params[:password])
+            #creating a key/value pair in the session hash using the users id to actually log them in
+           session[:user_id] = user.id
+            #redirect user's profile(users show)'
+                redirect "/users/#{user.id}"
+            else 
+            #show an error message
+            redirect '/login'
+        end
+
+    end
+    get "/users/:id" do
+        "users show page!"
+    end
+end
